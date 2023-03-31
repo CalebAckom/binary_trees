4cfedc85b7b4cc3550b920992be594774e705a81
CalebAckom <calebackom@amalitech.com>
2023-03-31 14:11:37 +0000

    Heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,84 +1,148 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
+ * height - measures the height of a tree
  *
  * @tree: tree root
- * Return: size of the tree or 0 if tree is NULL;
+ * Return: height
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+int height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	int left = 0;
+	int right = 0;
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (tree == NULL)
+		return (-1);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
 }
 
 /**
- * tree_is_complete - checks if tree is complete
- *
- * @tree: pointer to the tree root
- * @i: node index
- * @cnodes: number of nodes
- * Return: 1 if tree is complete, 0 otherwise
- */
-int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
-{
-	if (tree == NULL)
-		return (1);
-
-	if (i >= cnodes)
-		return (0);
-
-	return (tree_is_complete(tree->left, (2 * i) + 1, cnodes) &&
-			tree_is_complete(tree->right, (2 * i) + 2, cnodes));
-}
-
-/**
- * binary_tree_is_complete - calls to tree_is_complete function
+ * binary_tree_is_perfect - checks if a binary tree is perfect
  *
  * @tree: tree root
- * Return: 1 if tree is complete, 0 otherwise
+ * Return: 1 if tree is perfect, 0 otherwise
  */
-int binary_tree_is_complete(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t cnodes;
+	if (tree && height(tree->left) == height(tree->right))
+	{
+		if (height(tree->left) == -1)
+			return (1);
 
-	if (tree == NULL)
-		return (0);
+		if ((tree->left && !((tree->left)->left) && !((tree->left)->right))
+		    && (tree->right && !((tree->right)->left) && !((tree->right)->right)))
+			return (1);
 
-	cnodes = binary_tree_size(tree);
+		if (tree && tree->left && tree->right)
+			return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
 
-	return (tree_is_complete(tree, 0, cnodes));
+	return (0);
 }
 
 /**
- * check_parent - checks if parent has a greater value than its childs
+ * swap - swaps nodes when child is greater than parent
  *
- * @tree: pointer to the node
- * Return: 1 if parent has a greater value, 0 otherwise
+ * @arg_node: parent node
+ * @arg_child: child node
+ * Return: no return
  */
-int check_parent(const binary_tree_t *tree)
+void swap(heap_t **arg_node, heap_t **arg_child)
 {
-	if (tree == NULL)
-		return (1);
+	heap_t *node, *child, *node_child, *node_left, *node_right, *parent;
+	int left_right;
 
-	if (tree->n > tree->parent->n)
-		return (0);
-
-	return (check_parent(tree->left) && check_parent(tree->right));
+	node = *arg_node, child = *arg_child;
+	if (child->n > node->n)
+	{
+		if (child->left)
+			child->left->parent = node;
+		if (child->right)
+			child->right->parent = node;
+		if (node->left == child)
+			node_child = node->right, left_right = 0;
+		else
+			node_child = node->left, left_right = 1;
+		node_left = child->left, node_right = child->right;
+		if (left_right == 0)
+		{
+			child->right = node_child;
+			if (node_child)
+				node_child->parent = child;
+			child->left = node;
+		}
+		else
+		{
+			child->left = node_child;
+			if (node_child)
+				node_child->parent = child;
+			child->right = node;
+		}
+		if (node->parent)
+		{
+			if (node->parent->left == node)
+				node->parent->left = child;
+			else
+				node->parent->right = child;
+		}
+		parent = node->parent, child->parent = parent;
+		node->parent = child, node->left = node_left;
+		node->right = node_right, *arg_node = child;
+	}
 }
 
 /**
- * binary_tree_is_heap - checks if an input tree is a Max Binary Heap
- *
- * @tree: pointer to the root of the tree
- * Return: 1 if tree is a Max Binary Heap, 0 otherwise
+ * heap_insert - function that inserts a value in Max Binary Heap
+ * @value: value to be inserted
+ * @root: tree root
+ * Return: pointer to the created node, or NULL on failure.
  */
-int binary_tree_is_heap(const binary_tree_t *tree)
+heap_t *heap_insert(heap_t **root, int value)
 {
-	if (!binary_tree_is_complete(tree))
-		return (0);
+	heap_t *new_node;
 
-	return (check_parent(tree->left) && check_parent(tree->right));
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
+
+	if (binary_tree_is_perfect(*root) || !binary_tree_is_perfect((*root)->left))
+	{
+		if ((*root)->left)
+		{
+			new_node = heap_insert(&((*root)->left), value);
+			swap(root, &((*root)->left));
+			return (new_node);
+		}
+		else
+		{
+			new_node = (*root)->left = binary_tree_node(*root, value);
+			swap(root, &((*root)->left));
+			return (new_node);
+		}
+	}
+
+	if ((*root)->right)
+	{
+		new_node = heap_insert(&((*root)->right), value);
+		swap(root, (&(*root)->right));
+		return (new_node);
+	}
+	else
+	{
+		new_node = (*root)->right = binary_tree_node(*root, value);
+		swap(root, &((*root)->right));
+		return (new_node);
+	}
+
+	return (NULL);
 }
